a93183e8f5115aa45a5dafbd8eacb8dd26b81b81
Joseph <joseph.yao.ruozhou@gmail.com>
2026-06-13 19:29:32 +0800

    :wrench: revert restart docker env after cucumber coverage run

--- e2e-tests/scripts/run-cucumber-with-backend-coverage.sh
+++ e2e-tests/scripts/run-cucumber-with-backend-coverage.sh
@@ -86,5 +86,5 @@ fi
 
 log INFO "32" "Backend coverage artifacts are ready"
 
-log INFO "34" "Starting mysql and dotnet containers again"
-(cd "${repo_root}" && BACKEND_ENABLE_COVERAGE=true docker compose up -d --build) >/dev/null
+#log INFO "34" "Starting mysql and dotnet containers again"
+#(cd "${repo_root}" && BACKEND_ENABLE_COVERAGE=true docker compose up -d --build) >/dev/null
